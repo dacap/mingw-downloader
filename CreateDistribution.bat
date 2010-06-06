f3191e2c90aadab7f361759fb454b2201a6eb05f
David Capello <davidcapello@gmail.com>
2010-06-06 14:21:43 -0300

    Added file to create distributions of MinGW Downloader.

---
+++ CreateDistribution.bat
@@ -0,0 +1,10 @@
+mkdir MinGW-Downloader
+mkdir MinGW-Downloader\tests
+copy 7za.exe MinGW-Downloader
+copy wget.exe MinGW-Downloader
+copy README.txt MinGW-Downloader
+copy MinGW-4.5-Downloader.bat MinGW-Downloader
+copy tests\test_program.c MinGW-Downloader\tests
+copy tests\test_program.cpp MinGW-Downloader\tests
+7za.exe a MinGW-Downloader-0.1.zip MinGW-Downloader
+pause
